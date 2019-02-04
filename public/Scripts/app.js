6f3d1e2e3fd4e09c78d849f740e94275c2695332
Keya <keyagandhi251994@gmail.com>
2019-02-04 17:48:14 -0500

    exc 3

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+/*Custom javascript*/ 
+//iife- imediately invoked func express
+(function(){
+    function Start(){
+    console.log(`%c App Started...`, "font-size:20 px; color:blue; font-weight:bold")
+
+    }
+
+window.addEventListener("load",Start);}
+)();
